137ebf3a21df25520ab23db1b8647016a8ebbeb9
Asa Calow <asacalow@gmail.com>
2014-03-12 13:55:27 +0000

    Vanilla Yeoman-scaffolded Backbone application.

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global explorer, $*/
+
+
+window.explorer = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    explorer.init();
+});
